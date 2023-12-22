b74beace2938f1167b495f66709f69026953b1aa
eddDevDz95 <houedd608@gmail.com>
2023-12-22 12:28:38 +0100

    enabling analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import Footer from "../components/Footer";
 import siteMetadata from "../utils/siteMetaData";
 import Script from "next/script";
 import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
@@ -92,6 +92,7 @@ export default function RootLayout({ children }) {
         {children}
         <Footer />
         <SpeedInsights/>
+        <Analytics />
       </body>
     </html>
   );
