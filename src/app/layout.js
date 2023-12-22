2436c34055fe0b5dcd59889500348001b619488a
eddDevDz95 <houedd608@gmail.com>
2023-12-22 14:48:01 +0100

    add anylitics

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,7 @@ import siteMetadata from "../utils/siteMetaData";
 import Script from "next/script";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from '@vercel/analytics/react';
+
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
