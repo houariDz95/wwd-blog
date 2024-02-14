26a4753cdf98a59ed2c7b24b59bd0aca4d1c938b
eddDevDz95 <houedd608@gmail.com>
2024-02-14 12:38:07 +0100

    new commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -76,7 +76,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/*<script src="https://alwingulla.com/88/tag.min.js" data-zone="41324" async data-cfasync="false"></script>*/}
+        <script src="https://alwingulla.com/88/tag.min.js" data-zone="41324" async data-cfasync="false"></script>
       </head>
       <body
         className={cx(
