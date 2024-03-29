6190f2b18ecbffabf42291616ae840bb48fd9deb
eddDevDz95 <houedd608@gmail.com>
2024-03-29 03:03:30 +0100

    new commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -120,7 +120,7 @@ export default function RootLayout({ children }) {
   }`}
         </Script>
         <Script async="async" data-cfasync="false" src="//pl21991331.toprevenuegate.com/7d56700dd5a27d1bff2cf4b9a2774565/invoke.js" />
-        <Script type='text/javascript' src='//pl22341088.profitablegatecpm.com/2d/b3/dc/2db3dc1290e9032f99ff2952bb01f197.js' />
+        <script type='text/javascript' src='//pl22341088.profitablegatecpm.com/2d/b3/dc/2db3dc1290e9032f99ff2952bb01f197.js'></script>
         <Header />
         {children}
         <Footer />
