55f8e2818736fc5ad41615b7135168bb851283fb
eddDevDz95 <houedd608@gmail.com>
2024-03-31 16:35:30 +0100

    new commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -109,7 +109,7 @@ export default function RootLayout({ children }) {
         className={cx(
           inter.variable,
           manrope.variable,
-          "font-mr bg-light dark:bg-dark"
+          "font-mr bg-light dark:bg-dark  overflow-clip"
         )}
       >
         <Script id="theme-switcher" strategy="beforeInteractive">
