6408462214582987f832297269ceb6392a8eb59e
eddDevDz95 <houedd608@gmail.com>
2024-04-01 02:05:55 +0100

    new commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -77,7 +77,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
        {/*<script async="async" data-cfasync="false" src="//thubanoa.com/1?z=7088299"></script>*/}
-       <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=7236778"></script> 
+       {/* <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=7236778"></script>  */}
        {/* <Script
             id="in-push"
             dangerouslySetInnerHTML={{
