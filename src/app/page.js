c06dc1c406226d0cde1b81fc2b41da21def5f240
eddDevDz95 <houedd608@gmail.com>
2024-04-09 16:45:02 +0100

    new commit

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center">
       <HomeCoverSection blogs={allBlogs} />
       <FeaturedPosts blogs={allBlogs} />
-      <div className="max-w-[720px] mx-auto mt-8 overflow-hidden">
+      <div className="w-full mt-8 overflow-x-hidden">
         <Banner_720 />
       </div>
       <RecentPosts blogs={allBlogs} />
