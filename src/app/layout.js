c7326429ce8e3cb78a950154a1a8c9872fc997a8
eddDevDz95 <houedd608@gmail.com>
2024-04-26 14:52:08 +0100

    new commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -76,8 +76,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
        <head>
-      {/* <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=7306424"></script>
-        <Script
+      <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=7306424"></script>
+        {/*<Script
         id="vignet"
           dangerouslySetInnerHTML={{
             __html: `
@@ -90,7 +90,7 @@ export default function RootLayout({ children }) {
             `,
           }}
         />   */}
-       {/* <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=7344634"></script>  */}
+       <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=7344634"></script>  
       </head> 
       <body
         className={cx(
