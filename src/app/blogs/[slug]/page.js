73cfb0201081264edc7efb4abb1d92ec0d50a14f
eddDevDz95 <houedd608@gmail.com>
2024-05-13 20:22:05 +0100

    new update

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -97,7 +97,7 @@ export default function BlogPage({ params }) {
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
       
- <Script type='text/javascript' src='//pl22931104.profitablegatecpm.com/27/7c/cc/277cccb139255375f748ddfc6b9d01de.js' /> 
+ {/* <Script type='text/javascript' src='//pl22931104.profitablegatecpm.com/27/7c/cc/277cccb139255375f748ddfc6b9d01de.js' />  */}
       <article> 
       <div className="mb-8 text-center relative w-full h-[70vh] bg-dark">
         <div className="w-full z-10 flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
