30ac82120a2b6f99ee7f89f52216a2c6a880db69
eddDevDz95 <houedd608@gmail.com>
2024-06-09 14:12:04 +0100

    new commit

--- src/components/Blog/RenderMdx.js
+++ src/components/Blog/RenderMdx.js
@@ -9,6 +9,7 @@ import Banner_350 from '@/banners/banner_350'
 import Banner_160 from '@/banners/banner_160'
 import Link from 'next/link'
 import { IoMdCloudDownload } from "react-icons/io";
+import { useRouter } from 'next/navigation'
 const mdxComponents = {
     Image
 }
@@ -16,7 +17,7 @@ const mdxComponents = {
 const RenderMdx = ({blog}) => {
 
     const MDXContent = useMDXComponent(blog.body.code)
-
+    const router = useRouter()
   return (
     <div className='col-span-12  lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max
     prose-blockquote:bg-accent/20 
@@ -40,10 +41,12 @@ const RenderMdx = ({blog}) => {
 
     '>  
        <div className='mb-10 overflow-hidden'>
-          <Banner_480 />
+          <Banner_720 />
   </div> 
         <MDXContent components={mdxComponents}/>
-    <button className='px-4 py-2 w-full bg-green-500 text-white rounded-md flex flex-col items-center justify-center gap-3'>
+    <button 
+      onClick={() => router.push("https://www.highcpmgate.com/fc312mr9?key=52b71d4fcaa9fbe52ce02a852df7f7d9")}
+    className='px-4 mt-6 py-2 w-full bg-green-500 text-white rounded-md flex flex-col items-center justify-center gap-3'>
         {/*<Script async="async" data-cfasync="false" src="//pl21991331.profitablegatecpm.com/7d56700dd5a27d1bff2cf4b9a2774565/invoke.js" />
         <div id="container-7d56700dd5a27d1bff2cf4b9a2774565"></div>*/}
           <span className='text-xl font-semibold tracking-wide'>شارك واربح  1000$</span>
