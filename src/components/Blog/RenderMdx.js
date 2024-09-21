d0e2814ee7e93b7fd819b87b853178a08bc31478
eddDevDz95 <houedd608@gmail.com>
2024-09-21 12:02:18 +0100

    new commit

--- src/components/Blog/RenderMdx.js
+++ src/components/Blog/RenderMdx.js
@@ -17,7 +17,7 @@ const RenderMdx = ({blog}) => {
     const MDXContent = useMDXComponent(blog?.body?.code)
     const router = useRouter()
   return (
-    <div className='col-span-12  lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max
+    <div style={{direction: "ltr"}} className='col-span-12  lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max
     prose-blockquote:bg-accent/20 
     prose-blockquote:p-2
     prose-blockquote:px-6
