ebc9d4a7d799df82d18ea5fc098d433b8e6294eb
eddDevDz95 <houedd608@gmail.com>
2024-09-30 17:41:40 +0100

    new commit

--- src/app/landing-page/page.jsx
+++ src/app/landing-page/page.jsx
@@ -19,12 +19,12 @@ const LandingPage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center min-h-screen bg-dark absolute top-0 left-0 max-w-screen w-full z-50">
+    <div className="py-5 flex flex-col items-center min-h-screen bg-dark absolute top-0 left-0 max-w-screen w-full z-50">
       <div className="w-full flex items-center justify-center my-2">
         <Banner_720 />
       </div>
       <div style={{direction: "ltr"}} className="flex items-center justify-center h-full">
-        <div className="bg-whisqate p-8 rounded-lg shadow-lg max-w-md w-full">
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
           <h1 className="text-3xl font-bold text-center mb-4">
             How To Make An Additional $1,000 - $3,000 Per Day In Passive Income On Complete Autopilot In 2024...
           </h1>
